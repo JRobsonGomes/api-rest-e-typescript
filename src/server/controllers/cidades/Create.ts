ef236d8cd32ad22700eccfbe4ae7ef2afffa8ede
JRobsonGomes <robsong200@gmail.com>
2025-05-01 21:54:49 -0300

    feat: refatorar interfaces de validação e adicionar modelo ICidade

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -2,14 +2,12 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware/Validation';
+import { ICidade } from '../../database/models';
 
-interface ICidade {
-  id?: number;
-  nome: string;
-}
+interface IBodyProps extends Omit<ICidade, 'id'> {}
 
 export const createValidation = validation((getSchema) => ({
-  body: getSchema<ICidade>(
+  body: getSchema<IBodyProps>(
     yup.object().shape({
       id: yup.number().optional(),
       nome: yup.string().required().min(3).max(50),
@@ -18,7 +16,7 @@ export const createValidation = validation((getSchema) => ({
 }));
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
   const { body } = req;
   console.log(body);
 
--- src/server/controllers/cidades/UpdateById.ts
+++ src/server/controllers/cidades/UpdateById.ts
@@ -2,14 +2,13 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware/Validation';
+import { ICidade } from '../../database/models';
 
 interface IParamProps {
   id?: number;
 }
 
-interface IBodyProps {
-  nome: string;
-}
+interface IBodyProps extends Omit<ICidade, 'id'> {}
 
 export const updateByIdValidation = validation((getSchema) => ({
   params: getSchema<IParamProps>(
